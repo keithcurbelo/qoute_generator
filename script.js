22f018c1cc470361fc4e59c92fc1fc7abe8a6eed
Keith Curbelo <keithcurbelo@yahoo.com>
2020-08-19 12:22:58 -0400

    fetch quotes from api

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("running");
+//Get quote from api
+const getQuote = async () => {
+  const proxyUrl = "https://api.allorigins.win/get?url=";
+  const apiUrl =
+    "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const response = await fetch(proxyUrl + encodeURIComponent(apiUrl));
+    const data = await response.json();
+    console.log(JSON.parse(data.contents));
+  } catch (error) {
+    // getQuote();
+    console.log("No quote", error);
+  }
+};
+
+//On load
+getQuote();
