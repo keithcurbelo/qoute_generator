6a993a921d67a94626d20e8de82ffaedf3c54412
Keith Curbelo <keithcurbelo@yahoo.com>
2020-08-19 12:41:13 -0400

    add button functionality to tweet & new quote

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+
 //Get quote from api
 const getQuote = async () => {
   const proxyUrl = "https://api.allorigins.win/get?url=";
@@ -5,13 +11,33 @@ const getQuote = async () => {
     "http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
   try {
     const response = await fetch(proxyUrl + encodeURIComponent(apiUrl));
-    const data = await response.json();
-    console.log(JSON.parse(data.contents));
+    const dataEncoded = await response.json();
+    const data = JSON.parse(dataEncoded.contents);
+    //Author isn't always known
+    authorText.innerText =
+      data.quoteAuthor === "" ? "Unknown" : data.quoteAuthor;
+    //Reduce font size for long quote
+    if (data.quoteText.length > 120) quoteText.classList.add("long-quote");
+    else {
+      quoteText.classList.remove("long-quote");
+    }
+    quoteText.innerText = data.quoteText;
   } catch (error) {
     // getQuote();
     console.log("No quote", error);
   }
 };
 
+//Tweet the Quote
+const tweetQuote = () => {
+  const quote = quoteText.innerText;
+  const author = authorText.innerText;
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+  window.open(twitterUrl, "_blank");
+};
+
+//Event Listener
+newQuoteBtn.addEventListener("click", getQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 //On load
 getQuote();
